8273b017f2be51931c7dbfe52afeeb0b3ff84326
OLEANG <wannachok.job@gmail.com>
2024-10-03 13:15:27 +0700

    oleang: answer-for-javascript-conditional-logic

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,23 @@
 //Exercise 4
 // Switch Statement
 let lightBulbStatus = "On";
-
+let statusMessage ;
 //Start coding here
+
+switch (lightBulbStatus) {
+  case "On":
+     statusMessage = "Light bulb is On.";
+     break;
+  case "Off":
+     statusMessage = "Light bulb is Off.";
+    break;
+  case "Broken":
+     statusMessage = "Light bulb is Broken.";
+     break;
+  default:
+     statusMessage = "Please choose the correct input (On/Off/Broken) ";
+     break;
+};
+
+console.log( statusMessage);
+
